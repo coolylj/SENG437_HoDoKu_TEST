23c6e3d47c87abf2e4f5a36dcacf51922ef9df1c
Nick <nicolasvalera887@gmail.com>
2013-02-07 11:30:36 -0700

    Updated a little bit, now includes the way we can print out the currently generated sudoku. There are not many tests we can run on the generation that I can think of, but if we're creative maybe we can come up with a few more. All I can think of so far is testing for a valid generation, testing that creating a sudoku from a string works properly, and yeah that's pretty much it.

--- unit_test/SudokuGeneratorTests.java
+++ unit_test/SudokuGeneratorTests.java
@@ -17,20 +17,20 @@ public class SudokuGeneratorTests {
 		/*This works, but I don't know how we can use this. Also, Only the generation
 		 * part works for sure, I'm not sure if its getting the solutions properly */
 		
-		//SudokuGenerator generator = SudokuGeneratorFactory.getInstance();
-		//Sudoku2 s = generator.generateSudoku(false);
-		//generator.getNumberOfSolutions(s);
-		//System.out.println(s.getSolution());
+		SudokuGenerator generator = SudokuGeneratorFactory.getInstance();
+		Sudoku2 s = generator.generateSudoku(false);
+		generator.getNumberOfSolutions(s);
+		System.out.println(s.getSudoku(ClipboardMode.VALUES_ONLY));
+		System.out.println(s.getSudoku(ClipboardMode.VALUES_ONLY_FORMATTED));
 		
 		
 		/*This DOESN'T work*/
-		DifficultyLevel d = new DifficultyLevel(DifficultyType.EASY, 1000, "Test", Color.BLACK, Color.WHITE);
-		GenerateSudokuProgressDialog g = new GenerateSudokuProgressDialog(new java.awt.Frame(), false, d, GameMode.PLAYING);
-		BackgroundGenerator b = new BackgroundGenerator();
+		//DifficultyLevel d = new DifficultyLevel(DifficultyType.EASY, 1000, "Test", Color.BLACK, Color.WHITE);
+		//GenerateSudokuProgressDialog g = new GenerateSudokuProgressDialog(new java.awt.Frame(), false, d, GameMode.PLAYING);
+		//BackgroundGenerator b = new BackgroundGenerator();
 		//g.run();
-		Sudoku2 s = b.generate(d, GameMode.PLAYING, null);
+		//Sudoku2 s = b.generate(d, GameMode.PLAYING, null);
 		//System.out.println(s);
-
 		assertEquals("Sudoku failed to generate.",true, s.checkSudoku());
 		
 	}
