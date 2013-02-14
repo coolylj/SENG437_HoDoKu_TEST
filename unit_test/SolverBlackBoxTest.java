1746d04e4c0cb962390e4d4e5883754fa013446c
LJ <coolylj@gmail.com>
2013-02-13 22:54:15 -0700

    re-factored solver tests

--- unit_test/SolverBlackBoxTest.java
+++ unit_test/SolverBlackBoxTest.java
@@ -1,19 +1,17 @@
 import static org.junit.Assert.*;
 
-import java.awt.Color;
-
 import solver.*;
 import sudoku.*;
-import generator.*;
+//import generator.*;
 
 import org.junit.Test;
-import java.lang.*;
+import java.lang.String;
 
-public class SolverBlackBoxTest {
-		// easy 
+public class SolverBlackBoxTest {	
+		// easy game
 		String g1 = new String("..5......4....2.1...2.548...246....1...92....9.7...3.6............7..4...8..3.79.");
 		String s1 = new String("195867234478392615362154879524673981836921547917548326749286153253719468681435792");
-		// medium
+		// medium 
 		String g2 = new String("1.25..4.6.8..49.155........7..46.2..4......38.....3...6.8....4....1.4..9........2");
 		String s2 = new String("192537486386249715547681923735468291429715638861923574658392147273154869914876352");
 		// hard
@@ -26,52 +24,69 @@ public class SolverBlackBoxTest {
 		String g5 = new String(".6....9....23..8..3...4.1..74.6.9..1.......6...9.325....6..5....589.....1........");
 		String s5 = new String("867521934412397856395846172743659281521478369689132547936715428258964713174283695");
 		
-		private boolean testGame(String gameString, String expectedSolution)
+		Sudoku2 game;
+		SudokuSolver solver;	
+		
+		private void setup()
 		{
+			game = new Sudoku2();
+			solver = SudokuSolverFactory.getInstance();			
+		}
+		
+		private void teardown()
+		{
+			SudokuSolverFactory.giveBack(solver);
+			game = null;
+			solver = null;
+		}
+		
+		private boolean solveGame(String gameString, String expectedSolution)
+		{
+			setup();
+			
 			// create new game
-			Sudoku2 game = new Sudoku2();
 			game.setSudoku(gameString);
 			assertTrue(game.isSolved() == false);
 			System.out.printf("unsolved: %s\n", game.getSudoku(ClipboardMode.VALUES_ONLY));
 			
-			// get solver
-			SudokuSolver solver = SudokuSolverFactory.getInstance();
+			// solve it using all method(no restriction)
 			solver.setSudoku(game);
 			solver.solve();
-			SudokuSolverFactory.giveBack(solver);
-
+			
 			// check result
 			assertTrue(game.isSolved() == true);
-			String solution = game.getSudoku(ClipboardMode.VALUES_ONLY);
-			System.out.printf("  solved: %s\n", solution);
+			String solverSolution = game.getSudoku(ClipboardMode.VALUES_ONLY);
+			System.out.printf("  solved: %s\n", solverSolution);
 			
-			if(expectedSolution.equals(solution)) return true;
+			teardown();
+			
+			if(expectedSolution.equals(solverSolution)) return true;
 			else return false;
 		}
 		
 		@Test
 		public void testSolveEasy()
 		{
-			assertTrue(testGame(g1, s1));
+			assertTrue(solveGame(g1, s1));
 		}
 		@Test
 		public void testSolveMedium()
 		{
-			assertTrue(testGame(g2, s2));
+			assertTrue(solveGame(g2, s2));
 		}
 		@Test
 		public void testSolveHard()
 		{
-			assertTrue(testGame(g3, s3));
+			assertTrue(solveGame(g3, s3));
 		}
 		@Test
 		public void testSolveUnfair()
 		{
-			assertTrue(testGame(g4, s4));
+			assertTrue(solveGame(g4, s4));
 		}
 		@Test
 		public void testSolveExtreme()
 		{
-			assertTrue(testGame(g5, s5));
+			assertTrue(solveGame(g5, s5));
 		}	
 }
