5421066949a02f13ff016e593abd3762cd4b3a3e
LJ <coolylj@gmail.com>
2013-04-04 13:52:22 -0600

    trollolololol

--- src/solver/Integration_AbstractSolverTest.java
+++ src/solver/Integration_AbstractSolverTest.java
@@ -7,55 +7,58 @@ import sudoku.*;
 public class Integration_AbstractSolverTest {
 	private Sudoku2 sudoku;
 	private SudokuStepFinder finder;
-	
-    private AbstractSolver[] solvers;
+
     private BruteForceSolver bruteForceSolver;
     private GiveUpSolver giveUpSolver;
     
-    private SolutionStep ss;
+    private SolutionStep ss_finder;
+    private SolutionStep ss_solver;
     
 	@Before
 	public void setUp() throws Exception {
     	finder = new SudokuStepFinder();
-    	sudoku = new Sudoku2();
-    	
+    	sudoku = new Sudoku2();	
+  	
 		String g1 = new String("..5......4....2.1...2.548...246....1...92....9.7...3.6............7..4...8..3.79.");
 		sudoku.setSudoku(g1);
 		finder.setSudoku(sudoku);
 		
 		bruteForceSolver = new BruteForceSolver(finder);
 		giveUpSolver = new GiveUpSolver(finder);
-		solvers = new AbstractSolver[]{bruteForceSolver, giveUpSolver};
 		
-		ss = null;
+		ss_finder = null;
+		ss_solver = null;
 	}
 
-    @Test
-    public void test1_withType1(){
-    	ss = solvers[0].getStep(SolutionType.GIVE_UP);
-    	assertNull(ss);
-    }
-    
-    @Test
-    public void test2_withType2(){
-    	ss = solvers[0].getStep(SolutionType.BRUTE_FORCE);
-    	assertNotNull(ss);
-    	assertTrue(solvers[0].doStep(ss));
-    	assertFalse(solvers[1].doStep(ss));
-    }
-    
-    @Test
-    public void test3_withType2(){
-    	ss = solvers[1].getStep(SolutionType.BRUTE_FORCE);
-    	assertNull(ss);
-    }    
-    @Test
-    public void test4_withType1(){	
-    	ss = solvers[1].getStep(SolutionType.GIVE_UP);
-    	assertNotNull(ss);
-    	assertTrue(solvers[1].doStep(ss));
-    	assertFalse(solvers[0].doStep(ss));
-    }
-    
- 
+	@Test
+	public void test1(){
+		ss_finder = finder.getStep(SolutionType.GIVE_UP);
+		ss_solver = giveUpSolver.getStep(SolutionType.GIVE_UP);
+		assertTrue(ss_solver.isEqual(ss_finder));
+	} 
+	
+	@Test
+	public void test2(){
+		ss_finder = finder.getStep(SolutionType.BRUTE_FORCE);
+		ss_solver = bruteForceSolver.getStep(SolutionType.BRUTE_FORCE);
+		assertTrue(ss_solver.isEqual(ss_finder));
+	} 
+	
+	@Test
+	public void test3(){
+		int before = finder.getSudoku().getUnsolvedCellsAnz();
+		ss_finder = finder.getStep(SolutionType.GIVE_UP);
+		finder.doStep(ss_finder);
+		int after = finder.getSudoku().getUnsolvedCellsAnz();
+		assertEquals(after, before); // it does not actually solve any steps, since it "gives up"
+	}
+	
+	@Test
+	public void test4(){
+		int before = finder.getSudoku().getUnsolvedCellsAnz();
+		ss_finder = finder.getStep(SolutionType.BRUTE_FORCE);
+		finder.doStep(ss_finder);
+		int after = finder.getSudoku().getUnsolvedCellsAnz();
+		assertEquals(after, before-1);
+	}
 }
